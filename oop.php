563f5eb6fc7ffcbf6dad5e5e787cda11378ae2dd
skaffroddin <skaffroddin4@gmail.com>
2025-04-07 16:14:26 +0530

    oop

---
+++ oop.php
@@ -0,0 +1,24 @@
+
+<<<<<<<<<<<< OOPS Question And Answer >>>>>>>>>>>
+
+What is Object-Oriented Programming? How is it different from Procedural Programming?
+What are the main principles of OOP?
+Explain Encapsulation, Inheritance, Polymorphism, and Abstraction.
+What is a class and an object in PHP? Provide an example.
+What is the difference between public, private, and protected access modifiers in PHP?
+What is a constructor and destructor in PHP? Give an example?
+What is inheritance? How does PHP support inheritance?
+Can PHP support multiple inheritance? If not, how can it be achieved?
+What is the difference between an abstract class and an interface in PHP?
+Can a class implement multiple interfaces?
+When would you use an interface over an abstract class?
+What are static properties and methods? When would you use them?
+What is the self vs this keyword in PHP OOP?
+What is late static binding in PHP?
+What is the purpose of the final keyword in PHP?
+What is a trait in PHP and why is it used?
+What is dependency injection? How can it be implemented in PHP?
+What are magic methods in PHP? Name a few and their purpose (__get, __set, __call, __toString, etc.)
+What is method overriding in PHP? How does it work in inheritance?
+Does PHP support method overloading like Java? Why or why not?
+How can you achieve polymorphism in PHP?
